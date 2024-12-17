76f68a368c9799b843b710b961ab2bbf5132c6b0
Filip Mugosa <filipmugosa7@gmail.com>
2024-12-17 22:40:15 +0100

    Povrsina valjka

---
+++ src/main/java/PovrsinaValjka.java
@@ -0,0 +1,13 @@
+public class PovrsinaValjka {
+    public static void main(String[] args) {
+
+        double PI = 3.14;
+        double r = 10;
+        double H = 50;
+
+        double result = r * r * PI * H;
+
+        System.out.println(result);
+
+    }
+}
